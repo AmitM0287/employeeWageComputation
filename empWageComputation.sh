86ea404faf5c7a84a1510885df022bf10c93bb73
AmitM0287 <amitmanna0287@gmail.com>
2020-11-04 13:15:46 +0530

    check employee attendence

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,16 @@
 #! /bin/bash
 #Welcome Messege
 echo "Welcome to Employee Wage Computation Program!"
+
+#Check employee present or absent
+attendence()
+{
+        attendence=$((RANDOM % 2))
+
+        if [ $attendence -eq 1 ]
+        then
+                return 1        #Present
+        else
+                return 0        #Absent
+        fi
+}
