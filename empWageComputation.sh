69911ee128f685a466982d3b9bfcd66e9d1e5a92
AmitM0287 <amitmanna0287@gmail.com>
2020-11-04 13:31:33 +0530

    calculating wages

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -21,3 +21,17 @@ halfDay=4
 totalWage=0
 dailyWage=0
 
+ 	attendence
+        n=$?
+        if [ $n -eq 1 ]
+        then
+                t=$((RANDOM % 2))
+                if [ $t -eq 1 ]
+                then
+                        dailyWage=`expr $fullDay \* 20`
+                else
+                        dailyWage=`expr $halfDay \* 20`
+                fi
+        else
+                dailyWage=0
+        fi
